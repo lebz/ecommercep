1aeefd303747b675cdba4a5093f7f3db1e91b203
lebogang <lebogang@itschools.co.za>
2014-05-10 05:49:36 +0200

    heroku precompile error

--- websites/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Websites::Application.config.secret_token = '2ab1e9a02be03246f3696e83aff04dd4e54478080f41014e860217874f5629871d4890f453dde202fd5a504a7a4b1ebddbc9ce13da8eddd59a56b0ebc51842da'
