09d12921ad355b5e475587b2b86a124be67ea2a9
lebogang <lebogang@itschools.co.za>
2014-05-27 16:25:26 +0200

    heroku fix

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ end
 
 module Websites
   class Application < Rails::Application
+  I18n.config.enforce_available_locales = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
